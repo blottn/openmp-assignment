5ddc9a2df9c6849dc97e7dc50412c96bb67ffba9
Nicholas Blott <blottn@tcd.ie>
2018-03-16 09:56:05 +0000

    added more functionality to number generator

--- gen.py
+++ gen.py
@@ -1,7 +1,14 @@
 import sys
 import random
 size = int(sys.argv[1])
-for i in range(0,size):
+if int(sys.argv[2]) == 1:	#ordered
+	for i in range(0,size):
+		print(i)
+elif int(sys.argv[2]) == 2: #reverse ordered
+	for i in range(0,size):
+		print(size - i)
+else:
+	for i in range(0,size): #random
 	#f.write(str(int(random.uniform(0,size))) + '\n') 
 	print(int(random.uniform(0,size)))
 
