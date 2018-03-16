c468125fd86cadf84391df757953f1efe17ab5ff
Nicholas Blott <blottn@tcd.ie>
2018-03-16 17:06:44 +0000

    optimised

--- serial_sort.c
+++ serial_sort.c
@@ -9,6 +9,17 @@ void serial_swap(int *x, int *y) {
 	*y = temp;
 }
 
+void insertion_sort(int * arr, int low, int high) {
+	for (int i = 1; i < high ; i++) {
+		int val = arr[i];
+		int j;
+		for (j = i; j > low && val < arr[j - 1] ; j--) {
+			arr[j] = arr[j - 1];
+		}
+		arr[j] = val;
+	}
+}
+
 
 int partition(int* arr, int low, int high) {
     int pivot_val = arr[low];
@@ -17,7 +28,7 @@ int partition(int* arr, int low, int high) {
 
     while (right >= left) {
     
-        while (left <= right && arr[left] <= pivot_val) {
+        while (left <= right && arr[left] < pivot_val) {
             left++;
         }
         while (left <= right && arr[right] >= pivot_val) {
@@ -25,7 +36,7 @@ int partition(int* arr, int low, int high) {
         }
 
         if (right >= left) {
-            serial_swap(arr + left ,arr + right );
+            serial_swap(arr + left ,arr + right);
         }
     }
     serial_swap(arr + right, arr + low);
@@ -33,10 +44,23 @@ int partition(int* arr, int low, int high) {
 }
 
 void serial_quicksort(int* arr, int low, int high) {
+	if (high - low < 30) { 	//quicksort is slower than insertion sort on fewer than 30 elements
+		insertion_sort(arr,low,high);
+	}
     if (low < high) {
         int pivot_index = partition(arr,low,high);
-        serial_quicksort(arr,low,pivot_index);
-        serial_quicksort(arr,pivot_index + 1,high);
+
+		int top = pivot_index - 1;
+		while (top > low && arr[top] == arr[pivot_index]) {
+			top--;
+		}
+
+		int bottom = pivot_index + 1;
+		while (bottom < high && arr[bottom] == arr[pivot_index]) {
+			bottom++;
+		}
+        serial_quicksort(arr,low,top);
+        serial_quicksort(arr,bottom,high);
     }
 }
 
@@ -50,7 +74,7 @@ int main(int argc, char ** argv) {
 	for (int i = 0 ; i < iterations ; i++) {
 		int * arr = get_ordered(size);
 		start();
-		serial_quicksort(arr,0, size -1);
+		serial_quicksort(arr,0, size);
 		times[i] = end();
 	}
 	double tot = 0;
